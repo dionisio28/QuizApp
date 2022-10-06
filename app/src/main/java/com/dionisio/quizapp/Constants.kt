08c82aaac8de19571cf577003d185fa59a19192c
Dionisio <dionisio@malteso.com>
2022-10-06 10:34:56 -0400

    Quis Questions and activity

---
+++ app/src/main/java/com/dionisio/quizapp/Constants.kt
@@ -0,0 +1,134 @@
+package com.dionisio.quizapp
+
+object Constants {
+
+    fun getQuestions() : ArrayList<Question> {
+        val questionsList = ArrayList<Question>()
+
+
+        val que1 = Question(
+            1,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_argetina,
+            "Argentina",
+            "Brazil",
+            "Australia",
+            "Andorra",
+            1,
+        )
+        questionsList.add(que1)
+
+
+        val que2 = Question(
+            1,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_brazil,
+            "Bolivia",
+            "Cameroon",
+            "Belarus",
+            "Brazil",
+            4,
+        )
+        questionsList.add(que2)
+
+        val que3 = Question(
+            1,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_canada,
+            "China",
+            "Canada",
+            "Denmark",
+            "Finland",
+            2,
+        )
+        questionsList.add(que3)
+
+        val que4 = Question(
+            1,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_jamaica,
+            "Colombia",
+            "Jamaica",
+            "Angola",
+            "Argentina",
+            2,
+        )
+        questionsList.add(que4)
+
+        val que5 = Question(
+            1,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_israel,
+            "Brazil",
+            "Iran",
+            "Israel",
+            "Georgia",
+            3
+        )
+        questionsList.add(que5)
+
+        val que6 = Question(
+            1,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_japan,
+            "Japan",
+            "Haiti",
+            "Guatemala",
+            "Ireland",
+            1
+        )
+        questionsList.add(que6)
+
+        val que7 = Question(
+            1,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_austria,
+            "Bahamas",
+            "Bangladesh",
+            "Austria",
+            "Bolivia",
+            3
+        )
+        questionsList.add(que7)
+
+        val que8 = Question(
+            1,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_australia,
+            "Cuba",
+            "Honduras",
+            "Barbados",
+            "Australia",
+            4
+        )
+        questionsList.add(que8)
+
+        val que9 = Question(
+            1,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_india,
+            "India",
+            "Iceland",
+            "Indonesia",
+            "Iraq",
+            1
+        )
+        questionsList.add(que9)
+
+        val que10 = Question(
+            1,
+            "What country does this flag belong to?",
+            R.drawable.ic_flag_italy,
+            "Italy",
+            "Hungary",
+            "France",
+            "Afghanistan",
+            1
+        )
+        questionsList.add(que10)
+
+
+        return  questionsList
+    }
+
+}
--- app/src/main/java/com/dionisio/quizapp/MainActivity.kt
+++ app/src/main/java/com/dionisio/quizapp/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.dionisio.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart = findViewById<Button>(R.id.btn_start)
+        val inputName = findViewById<EditText>(R.id.input_name)
+
+        btnStart.setOnClickListener {
+            if(inputName.text.toString().isNotEmpty()){
+
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish() // For close this activity
+
+            }else{
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
